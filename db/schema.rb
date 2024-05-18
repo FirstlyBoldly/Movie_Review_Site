8155b797061d455e284d0e3047a9901a175912c8
Death Salad <ankhbhold@gmail.com>
2024-05-18 15:02:29 +0900

    General Improvements

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_11_064733) do
   create_table "movies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "impression"
+    t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "likes"
   end
 
 end
