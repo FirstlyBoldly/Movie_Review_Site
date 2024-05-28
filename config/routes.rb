3afbcfde0bc704bf61d0a19e3e8e9d48007747a3
Death Salad <ankhbhold@gmail.com>
2024-05-28 14:18:54 +0900

    Big Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'master#index'
   # get 'master/index'
   get 'master/new'
   get 'master/:id' => 'master#show', as: 'master_show'
   get 'master/:id/edit' => 'master#edit', as: 'master_edit'
+  get 'users/index'
+  get 'users/:id' => 'users#details', as: 'users_details'
   post 'movies' => 'master#create'
-  patch 'movies/:id' => 'master#update', as: 'master_update'
-  delete 'movies/:id' => 'master#destroy', as: 'master_destroy'
+  patch 'master/:id' => 'master#update', as: 'master_update'
+  delete 'master/:id' => 'master#destroy', as: 'master_destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
